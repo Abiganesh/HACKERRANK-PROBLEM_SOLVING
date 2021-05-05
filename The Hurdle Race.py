f4106755056dce6dd93848f6012717c1abeacb02
Ganesh J <52096566+Abiganesh@users.noreply.github.com>
2021-05-05 17:57:39 +0530

    Add files via upload

---
+++ The Hurdle Race.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hurdleRace' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY height
+#
+
+def hurdleRace(k, height):
+    sor=sorted(height, reverse=True)
+    print(sor)
+    print(sor[0])
+    if(k<sor[0]):
+        ret=sor[0]-k
+    else:
+        ret=0
+    return ret
+
+if __name__ == '__main__':
+    
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    print(result)
